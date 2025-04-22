86afd91de0e5fd2f0414479d3744cf5368b1e42c
muhammad zhafran <mzhafranputrahartono@gmail.com>
2025-04-22 10:54:54 +0700

    Add files via upload

---
+++ login-smk/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - SMK Bunda Kandung</title>
+</head>
+<body>
+    <h2 align="center">Form Login</h2>
+    <form action="login.php" method="post" align="center">
+        <input type="text" name="username" placeholder="Username" required><br><br>
+        <input type="password" name="password" placeholder="Password" required><br><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
